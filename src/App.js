1a01195fd7375bc9cb7344ea25bdf236fbcf6785
tristan <77806722+trstngttschlk@users.noreply.github.com>
2023-07-27 21:06:59 -0400

    basic structure for city form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const API_KEY = "8752560f395246d7a25cc701074accaf";
+  const [city, setCity] = useState("");
+  useEffect;
+
+  // https://www.weatherbit.io/api/weather-current
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Please enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
     </div>
   );
 }
-
-export default App;
