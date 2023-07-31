42bd0dfdf3034bf628028f29d7a04dca20e24405
tristan <77806722+trstngttschlk@users.noreply.github.com>
2023-07-31 17:27:17 -0400

    switch to openweathermap api, additional work, basic app functionality works

--- src/App.js
+++ src/App.js
@@ -1,40 +1,62 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 
 export default function App() {
-  const API_KEY = "8752560f395246d7a25cc701074accaf";
-  const [city, setCity] = useState("Berlin");
-  const [cityData, setCityData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const API_KEY = "fc1f03d2ec00c00d9883ed491ace1e1a";
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-  useEffect(
-    function () {
-      async function fetchData() {
-        setIsLoading(true);
-        
-        const res = await fetch(
-          `http://api.weatherbit.io/v2.0/current/?key=${API_KEY}&city=${city}/`
-        );
+  const [longitude, setLongitude] = useState(0)
+  const [latitude, setLatitude] = useState(0)
 
-        const data = await res.json();
 
-        setCityData(data.data);
-        console.log(cityData);
-      }
+async function fetchLongLat() {
+  
+  const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`)
 
-      fetchData();
-    },
-    [city]
-  );
+  const data = await res.json()
+
+  setLongitude((data[0].lon))
+  setLatitude((data[0].lat))
+
+}
+
+async function fetchWeather() {
+
+  await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${API_KEY}`)
+  .then(res => res.json())
+  .then(data => setWeather(data))
+  
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+
+  fetchLongLat()
+
+  fetchWeather()
+
+}
+
+  
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Please enter city name"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      {isLoading ? <p>The temperature in {city} is: x</p> : ""}
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Please enter city name"
+          value={query}
+          onChange={(e)=>setQuery(e.target.value)}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+
+      {weather.main ? <p>the current temperature in {query[0].toUpperCase() + query.slice(1)} is {weather.main.temp} °F</p> : null}
+      
+      <h2>Coordinates</h2>
+      <p>Longitude: {longitude.toFixed(2)}</p>
+      <p>Latitude: {latitude.toFixed(2)}</p>
     </div>
   );
 }
